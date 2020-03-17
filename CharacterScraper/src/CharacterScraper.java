70dd5185a90837ab8f0fce7e46adac4a75c81b70
Alec Sweet <alecsweet@utexas.edu>
2020-03-17 16:41:10 -0500

    Support threading and task stopping

--- CharacterScraper/src/CharacterScraper.java
+++ CharacterScraper/src/CharacterScraper.java
@@ -8,9 +8,13 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -25,7 +29,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.By;
@@ -36,12 +39,17 @@ import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 
 public class CharacterScraper{
-	private static String[] races = {"Aasimar","Dragonborn","Dwarf","Elf","Firbolg","Gnome","Goblin","Goliath","Halfling","Half-Elf","Half-Orc","Human","Kenku","Lizardfolk","Medusa","Orc","Tabaxi","Tiefling","Triton","Troglodyte"};
-	private static String[] classNames = {"Learned","Lesser Nobility","Professional","Working Class","Martial","Underclass","Entertainer"};
 	private static String destPath = "";
-	private static JLabel error = new JLabel("Result: ");	
+	private static Future curFuture = null;
+	private static InputTask curTask = null;
+	
 	public static void main( String[] args ) throws IOException, InterruptedException
     {
+		startJframe();	
+    }
+	
+	private static void startJframe() {
+		ExecutorService taskExecutor = Executors.newFixedThreadPool(1);
 		
 		JFrame f = new JFrame("                                                                                    Literal Malware ʕ •ᴥ•ʔ");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -57,9 +65,10 @@ public class CharacterScraper{
 		JLabel image = new JLabel(new ImageIcon(i));
 		image.setBounds(25, 25, 650, 250);
 					
-		JTextField textfield = new JTextField("race : num ; p0 : p1 : p2 : p3 : p4 : p5 : p6 ,");
+		JTextField textfield = new JTextField("race : 10 ; 0 : 0 : 0 : 0 : 0 : 0 : 0 ,");
 		textfield.setBounds(60, 50 + 250, 590, 30);
 		
+		JLabel error = new JLabel("Result: ");	
 		error.setBounds(155 ,90 + 250, 550, 60);
 						
 		f.add(label);
@@ -97,89 +106,127 @@ public class CharacterScraper{
     	
     	f.addWindowListener(new WindowAdapter() {
 			  public void windowClosing(WindowEvent we) {
+				taskExecutor.shutdownNow();
 				driver.quit();
 			    System.exit(0);
 			  }
 			
 		});
     	
-    	String ref = "http://www.npcgenerator.com/";
-    	
-    	b.addActionListener(new ActionListener() {
-	        
+   	
+    	b.addActionListener(new ActionListener() {        
+			@SuppressWarnings("unchecked")
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				try {
-					String inp = textfield.getText();
-					inp = inp.replace(" ", "");
-					inp = inp.replace("\n", "");
-					String[] split = inp.split(",");
-					SXSSFWorkbook wb = getNewWorkBook();
-					tryGetDocument(ref, driver);
-					int rowNum = 1;
-					for(String s: split) {
-						String[] first = s.split(";");
-						String[] pair = first[0].split(":");
-						String[] percentages = first[1].split(":");			
-						int race = findRace(pair[0].toLowerCase().trim());
-						int num = 0;
-						num = Integer.parseInt(pair[1]);
-						double[] percen = new double[7];
-						for(int n = 0; n < percentages.length; n++) {
-							percen[n] = ((double)Integer.parseInt(percentages[n].trim())) / 100.0;
-						}
-						int[] cT = {0,0,0,0,0,0,0};
-						if(race != -1)						
-							for(int i = 0; i < num; i++){
-								int socialClass = getClassIndex(percen);
-								cT[socialClass]++;
-								writeRow(
-										wb.getSheetAt(0),
-										getCharacter(driver,race,socialClass),
-										rowNum
-								);
-								String res = "Getting " + races[race] + " number " + i + " / " + num + "   :  " + cT[0] + "| "+cT[1]+"| "+cT[2]+"| "+cT[3]+"| "+cT[4]+"| "+cT[5]+"| "+cT[6];
-								/*res += "Learned:\t\t" + cT[0]  + "\\n";
-								res += "Lesser Nobility:\t\t" + cT[1]  + "\\n";
-								res += "Professional:\t\t" + cT[2]  + "\\n";
-								res += "Working Class:\t\t" + cT[3]  + "\\n";
-								res += "Martial:\t\t" + cT[4]  + "\\n";
-								res += "Underclass:\t\t" + cT[5]  + "\\n";
-								res += "Entertainer:\t\t" + cT[6]  + "\\n";*/
-								error.setText(res);		
-								f.update(f.getGraphics());
-								rowNum++;
-							}
-						else {
-							error.setText("Error: Race Name Error");
-							f.update(f.getGraphics());
-						}		
-					}
-					String fileName = destPath + "\\ResultCharacterSet.xlsx";
-			    	try {
-			        	FileOutputStream excelOutputStream = new FileOutputStream(fileName);
-						wb.write(excelOutputStream);
-						excelOutputStream.close();
-						textfield.setText("race : num ; p0 : p1 : p2 : p3 : p4 : p5 : p6 ,");
-						error.setText("Success: at " + destPath + "\\ResultCharacterSet.xlsx");
-					} catch (IOException e) {
-						error.setText("Error: Excel File Already Open");
-					}			    
-			    	f.update(f.getGraphics());
-				}catch(NumberFormatException e) {
-					error.setText("Error: Number Input Error");
-					f.update(f.getGraphics());
-				}catch(IndexOutOfBoundsException e) {
-					error.setText("Error: Delim Input Error");
-					f.update(f.getGraphics());
-				}catch(WebDriverException e) {
-					driver.quit();
-				    System.exit(0);
+				if(curFuture == null || b.getText().equals("Submit")) {	
+					b.setText("Stop");
+					curTask = new InputTask(f, error, textfield.getText(), driver, destPath);
+					curFuture = taskExecutor.submit(curTask);
+					return;
+				}else if(curFuture != null && b.getText().equals("Stop")){
+					b.setText("Submit");
+					curTask.setCanceled(true);
+					curFuture.cancel(true);
+
+					textfield.setText("race : 10 ; 0 : 0 : 0 : 0 : 0 : 0 : 0 ,");				
 				}
-			} 	
+			}			
 		});  	
-    }
+	}
+}
+
+class InputTask implements Callable{
+	private static final String[] races = {"Aasimar","Dragonborn","Dwarf","Elf","Firbolg","Gnome","Goblin","Goliath","Halfling","Half-Elf","Half-Orc","Human","Kenku","Lizardfolk","Medusa","Orc","Tabaxi","Tiefling","Triton","Troglodyte"};
+	private static final String[] classNames = {"Learned","Lesser Nobility","Professional","Working Class","Martial","Underclass","Entertainer"};
+	private static final String ref = "http://www.npcgenerator.com/";
+	private boolean canceled = false;
+	private JFrame f;
+	private JLabel error;
+	private String text, destPath;
+	private WebDriver driver;
 	
+	public InputTask(JFrame frame, JLabel error, String text, WebDriver driver, String destPath) {
+		f = frame;
+		this.error = error;
+		this.text = text;
+		this.driver = driver;
+		this.destPath = destPath;
+	}
+	
+	public void setCanceled(boolean t){
+		canceled = t;
+	}
+	
+	@Override
+	public Integer call() {
+		SXSSFWorkbook wb = getNewWorkBook();
+		System.out.println("running1");
+		try {
+			String inp = text;//textfield.getText();
+			inp = inp.replace(" ", "");
+			inp = inp.replace("\n", "");
+			String[] split = inp.split(",");
+			tryGetDocument(ref, driver);
+			int rowNum = 1;
+			for(String s: split) {
+				System.out.println("running2");
+				String[] first = s.split(";");
+				String[] pair = first[0].split(":");
+				String[] percentages = first[1].split(":");
+				int race = findRace(pair[0].toLowerCase().trim());
+				if(percentages.length < 7){
+					throw new IllegalArgumentException("Error: Percentages incorrect for " + races[race]);
+				}
+				int num = 0;
+				num = Integer.parseInt(pair[1]);
+				double[] percen = new double[7];
+				for(int n = 0; n < percentages.length; n++) {
+					percen[n] = ((double)Integer.parseInt(percentages[n].trim())) / 100.0;
+				}
+				int[] cT = {0,0,0,0,0,0,0};
+				if(race != -1)						
+					for(int i = 0; i < num; i++){
+						System.out.println("running3");
+						int socialClass = getClassIndex(percen);
+						cT[socialClass]++;
+						writeRow(
+								wb.getSheetAt(0),
+								getCharacter(driver,race,socialClass),
+								rowNum
+						);
+						String res = "Getting " + races[race] + " number " + i + " / " + num + "   :  " + cT[0] + "| "+cT[1]+"| "+cT[2]+"| "+cT[3]+"| "+cT[4]+"| "+cT[5]+"| "+cT[6];
+						error.setText(res);		
+						f.update(f.getGraphics());
+						rowNum++;
+						if(canceled)
+							throw new IllegalArgumentException("Task Stopped");
+					}
+				else 
+					throw new IllegalArgumentException("Error: Race Name Error");						
+			}
+			
+			outputExcel(wb,"Success");
+		}catch(NumberFormatException e) {
+			error.setText("Error: Number Input Parsing Error");
+		}catch(IndexOutOfBoundsException e) {
+			error.setText("Error: Delim Input Parsing Error");
+		}catch(IllegalArgumentException e){
+			error.setText(e.getMessage());
+			switch(e.getMessage()) {
+				case "Task Stopped": 
+					outputExcel(wb,"Stopped");
+					break;
+				case "Connection Dead": 
+					outputExcel(wb,"ConnectionLost");
+					break;
+			}
+		}catch(WebDriverException e) {
+			driver.quit();
+		    System.exit(0);
+		}
+		f.update(f.getGraphics());
+		return new Integer(1);
+	} 
 	private static int findRace(String s) {
 		int num = 0;
 		for(String r: races) {
@@ -301,34 +348,55 @@ public class CharacterScraper{
 	    		driver.get(absRef);
 	    		retry = false;
 	    	}catch(TimeoutException e) {
-	    		retry = true;
-	    		error.setText("Retrying Connection, Timeout In: " + (System.currentTimeMillis() - timeout) + "ms");
+	    		retry = true;	  
 	    	}
 	    	if(System.currentTimeMillis() - timeout > 60000)
 	    		break;
     	}
+    	if(retry)
+    		throw new IllegalArgumentException("Connection Dead");
     	rest();
     }
 	
-	private static void rest()
-    {
+    private static SXSSFWorkbook getNewWorkBook(){
+			SXSSFWorkbook workbook = new SXSSFWorkbook();	  
+			SXSSFSheet characters = workbook.createSheet("Characters");
+		    Row header = characters.createRow(0);	  
+		    header.createCell(0).setCellValue("Name");
+		    header.createCell(1).setCellValue("Race");
+		    header.createCell(2).setCellValue("Age");
+		    header.createCell(3).setCellValue("Class");
+		    header.createCell(4).setCellValue("Profession");
+	        header.createCell(5).setCellValue("Description");
+	        header.createCell(6).setCellValue("Backstory");
+	        header.createCell(7).setCellValue("Hook");
+	        return workbook;
+	   }
+    
+	private static void rest(){
     	long targetTime = (long)(Math.random() * 200 + 200) + System.currentTimeMillis();
-    	while(targetTime > System.currentTimeMillis()){
-    	}
+    	while(targetTime > System.currentTimeMillis()){}
     }
 	
-   private static SXSSFWorkbook getNewWorkBook(){
-		SXSSFWorkbook workbook = new SXSSFWorkbook();	  
-		SXSSFSheet characters = workbook.createSheet("Characters");
-	    Row header = characters.createRow(0);	  
-	    header.createCell(0).setCellValue("Name");
-	    header.createCell(1).setCellValue("Race");
-	    header.createCell(2).setCellValue("Age");
-	    header.createCell(3).setCellValue("Class");
-	    header.createCell(4).setCellValue("Profession");
-        header.createCell(5).setCellValue("Description");
-        header.createCell(6).setCellValue("Backstory");
-        header.createCell(7).setCellValue("Hook");
-        return workbook;
-   }
+	private void outputExcel(SXSSFWorkbook wb, String extra) {
+		int attempt = 0;
+		boolean exit = false;
+		while(!exit) {
+	    	try {
+	    		String fileName;
+	    		if(attempt > 0)
+	    			fileName = destPath + "\\ResultCharacterSet" + extra + attempt + ".xlsx";
+	    		else
+	    			fileName = destPath + "\\ResultCharacterSet" + extra + ".xlsx";
+	        	FileOutputStream excelOutputStream = new FileOutputStream(fileName);
+	        	exit = true;
+				wb.write(excelOutputStream);
+				exit = true;
+				excelOutputStream.close();			
+				error.setText("Success: at " + fileName);
+			} catch (IOException e) {
+				attempt++;
+			}
+		}
+	}	    		
 }
